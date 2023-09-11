8cb5c4ad033fcd30807eee1f175f2ec7584c2dd7
Pedro Durães <120066201+pedroduraesdev@users.noreply.github.com>
2023-09-11 19:55:05 -0300

    Update ExJava2.java
    Realizando update

--- ExJava2.java
+++ ExJava2.java
@@ -2,9 +2,11 @@ public class ExJava2 {
     public static void main(String[] args) {
         int media1 = ((8 + 9 + 7) / 3);
         int media2 = ((4 + 5 + 6) / 3);
+        float soma = media1 + media2;
+        float media3 = media1 + media2/2
 
         System.out.println("A média dos números 8,9 e 7 é: " + media1);
         System.out.println("A média dos números 4,5 e 6 é: " + media2);
-        System.out.println("A média das duas médias é: " + (media1 + media2) / 2 );
+        System.out.println("A média das duas médias é: " + media3);
     }
 }
